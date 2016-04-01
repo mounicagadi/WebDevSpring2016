4f85802bd5d205bb386d934e632497e1bd913688
Mounica Gadi <gmounica93@ccs.neu.edu>
2016-04-01 19:04:00 -0400

    changed server

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ var mongoose = require("mongoose");
 var db = mongoose.connect('mongodb://127.0.0.1:27017/assignment');
 
 
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
